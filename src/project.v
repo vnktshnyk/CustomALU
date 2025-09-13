8de096f51155f87ef0020271a0288132a0a7591b
vnktshnyk <venkateshan.lvs23@rvce.edu.in>
2025-09-13 11:54:22 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,8 +18,8 @@ module tt_um_customalu (
 
   // All output pins must be assigned. If not used, assign to 0.
  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  //assign uio_out = 0;
-  //assign uio_oe  = 0;
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   //wire _unused = &{ena, clk, rst_n, 1'b0};
@@ -86,5 +86,5 @@ case (Opcode)
         end 
         endcase 
     end  
-    wire _unused = &{ena, clk, rst_n,uio_out,uio_oe, 1'b0}; 
+    wire _unused = &{ena, clk, rst_n, 1'b0}; 
 endmodule
